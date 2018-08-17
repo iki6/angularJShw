898d2877890b7c5a35ceefee2e1efcf68bfc6aaa
Ирина Чуркина <feel_good@MacBook-Pro-Irina.local>
2018-08-17 11:28:50 +0300

    some changes

--- Churkina-Irina/src/app.js
+++ Churkina-Irina/src/app.js
@@ -13,6 +13,14 @@ mainCtrlFunc.prototype.showCat = function() {
 	this.shownPet = 'Cat';
 };
 
+btnsApp.directive('petType', function(){
+	return {
+		controller: mainCtrlFunc,
+		controllerAs: 'ctrl',
+		template: '<span>This is a {{ctrl.shownPet}} :)</span>'
+	}
+})
+
 btnsApp.controller('mainCtrl', mainCtrlFunc);
 
 btnsApp.controller('dogCtrl', function($scope){
